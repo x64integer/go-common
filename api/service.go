8ebe717a41433d56f7d461a4282168f8cdca5077
Semir Mahovkic <semirmahovkic@Semirs-MacBook-Pro.local>
2019-02-26 12:29:27 +0100

    prototype for query builder

--- api/service.go
+++ api/service.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/x64integer/go-common/util"
+
+	"github.com/x64integer/go-common/password"
 	"github.com/x64integer/go-common/storage"
 )
 
@@ -15,23 +18,46 @@ type Service struct {
 // Register user account
 func (svc *Service) Register(fields []*entityField) ([]byte, error) {
 	var columns []string
-	var binders []string
+	var queryParams []string
 	var data []interface{}
 	queryBuff := bytes.NewBufferString("")
 
 	// construct query and data: "INSERT INTO users (id, username, email, password) VALUES ($1, $2, $3, $4)"
 	for i, field := range fields {
-		if field.AuthType == "auto_gen" {
+
+		// TODO: this is only prototype of what has to be done, will be changed
+		var dataValue interface{}
+		var param string
+
+		switch field.AuthType {
+		case "secret":
+			pwd, err := password.Hash(fmt.Sprint(field.AuthValue))
+			if err != nil {
+				return nil, err
+			}
+
+			dataValue = pwd
+		case "auto_gen":
 			continue
+		default:
+			dataValue = field.AuthValue
+		}
+
+		switch util.Env("DB_DRIVER", "") {
+		case strings.ToLower("mysql"):
+			param = "?"
+		default:
+			param = "$" + fmt.Sprint(i)
 		}
 
 		columns = append(columns, field.AuthKey)
-		binders = append(binders, "$"+fmt.Sprint(i))
-
-		data = append(data, field.AuthValue)
+		queryParams = append(queryParams, param)
+		data = append(data, dataValue)
 	}
 
-	queryBuff.WriteString("INSERT INTO " + strings.ToLower(fields[0].AuthTable) + "s (" + strings.Join(columns, ", ") + ") VALUES (" + strings.Join(binders, ", ") + ")")
+	queryBuff.WriteString(
+		"INSERT INTO " + strings.ToLower(fields[0].AuthTable) + "s (" + strings.Join(columns, ", ") + ") VALUES (" + strings.Join(queryParams, ", ") + ")",
+	)
 
 	if _, err := storage.PG.Exec(queryBuff.String(), data...); err != nil {
 		return nil, err
--- go.mod
+++
@@ -1,1 +0,0 @@
-module github.com/x64integer/go-common
