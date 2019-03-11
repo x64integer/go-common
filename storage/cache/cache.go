70647bb8fc5637abe3419c8019ca2f8920a3ef76
Semir Mahovkic <semir.x64@gmail.com>
2019-03-11 16:09:03 +0100

    updated cache service comments

--- storage/cache/cache.go
+++ storage/cache/cache.go
@@ -4,15 +4,15 @@ import "time"
 
 // Service ...
 type Service interface {
-	// Store item/s into cache
+	// Store item(s) into cache
 	Store(...*Item) error
-	// Get items from cache
+	// Get item(s) from cache
 	Get(...*Item) ([]byte, error)
-	// Delete item/s from cache
+	// Delete item(s) from cache
 	Delete(...*Item) error
-	// Truncate all stored items in cache
+	// Truncate all items from cache
 	Truncate() error
-	// Custom func to run against item/s
+	// Custom func to run against item(s)
 	Custom(func(...*Item) error, ...*Item) error
 }
 
